354a9a4d6b00e13ba766e8913cc4330a04a29feb
Eugene Trocenko <trocenko1999@gmail.com>
2025-01-30 15:09:06 +0300

    Ex 5

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,1 +1,53 @@
-﻿
+﻿function parseCount(value) {
+    const parsedValue = Number.parseFloat(value);
+    if (isNaN(parsedValue)) {
+        throw new Error("Невалидное значение");
+    }
+    return parsedValue;
+}
+
+function validateCount(value) {
+    try {
+        return parseCount(value);
+    } catch (error) {
+        return error;
+    }
+}
+
+console.log(validateCount("10")); 
+console.log(validateCount("abc")); 
+
+class Triangle {
+    constructor(a, b, c) {
+        if (a + b <= c || a + c <= b || b + c <= a) {
+            throw new Error("Треугольник с такими сторонами не существует");
+        }
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
+
+    get perimeter() {
+        return this.a + this.b + this.c;
+    }
+
+    get area() {
+        const s = this.perimeter / 2;
+        return +(Math.sqrt(s * (s - this.a) * (s - this.b) * (s - this.c))).toFixed(3);
+    }
+}
+
+function getTriangle(a, b, c) {
+    try {
+        return new Triangle(a, b, c);
+    } catch (error) {
+        return {
+            get perimeter() { return "Ошибка! Треугольник не существует"; },
+            get area() { return "Ошибка! Треугольник не существует"; }
+        };
+    }
+}
+
+console.log(getTriangle(3, 4, 5).perimeter); 
+console.log(getTriangle(3, 4, 5).area); 
+console.log(getTriangle(1, 1, 3).perimeter);
