844b31c9d8b7f131d3d031f5bbcc401f4c28d5c3
Eugene Trocenko <trocenko1999@gmail.com>
2025-01-30 14:27:54 +0300

    Ex 3

---
+++ 5.classes/task.js
@@ -0,0 +1,79 @@
+class PrintEditionItem {
+    constructor(name, releaseDate, pagesCount) {
+      this.name = name;
+      this.releaseDate = releaseDate;
+      this.pagesCount = pagesCount;
+      this.state = 100;
+      this.type = null;
+    }
+  
+    fix() {
+      this.state *= 1.5;
+      if (this.state > 100) this.state = 100;
+    }
+  
+    set state(value) {
+      if (value < 0) {
+        this._state = 0;
+      } else if (value > 100) {
+        this._state = 100;
+      } else {
+        this._state = value;
+      }
+    }
+  
+    get state() {
+      return this._state;
+    }
+  }
+  
+  class Magazine extends PrintEditionItem {
+    constructor(name, releaseDate, pagesCount) {
+      super(name, releaseDate, pagesCount);
+      this.type = "magazine";
+    }
+  }
+  
+  class Book extends PrintEditionItem {
+    constructor(author, name, releaseDate, pagesCount) {
+      super(name, releaseDate, pagesCount);
+      this.author = author;
+      this.type = "book";
+    }
+  }
+  
+  class NovelBook extends Book {
+    constructor(author, name, releaseDate, pagesCount) {
+      super(author, name, releaseDate, pagesCount);
+      this.type = "novel";
+    }
+  }
+  
+  class FantasticBook extends Book {
+    constructor(author, name, releaseDate, pagesCount) {
+      super(author, name, releaseDate, pagesCount);
+      this.type = "fantastic";
+    }
+  }
+  
+  class DetectiveBook extends Book {
+    constructor(author, name, releaseDate, pagesCount) {
+      super(author, name, releaseDate, pagesCount);
+      this.type = "detective";
+    }
+  }
+  
+  // Примеры использования
+  const picknick = new FantasticBook(
+    "Аркадий и Борис Стругацкие",
+    "Пикник на обочине",
+    1972,
+    168
+  );
+  
+  console.log(picknick.author); // "Аркадий и Борис Стругацкие"
+  picknick.state = 10;
+  console.log(picknick.state); // 10
+  picknick.fix();
+  console.log(picknick.state); // 15
+  
