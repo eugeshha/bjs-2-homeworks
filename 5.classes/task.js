2e42c7c549e57d7c475b655f6aab342c24200dd2
Eugene Trocenko <trocenko1999@gmail.com>
2025-01-30 14:36:03 +0300

    Ex 4

--- 5.classes/task.js
+++ 5.classes/task.js
@@ -63,17 +63,64 @@ class PrintEditionItem {
     }
   }
   
-  // Примеры использования
-  const picknick = new FantasticBook(
-    "Аркадий и Борис Стругацкие",
-    "Пикник на обочине",
-    1972,
-    168
+  class Library {
+    constructor(name) {
+      this.name = name;
+      this.books = [];
+    }
+  
+    addBook(book) {
+      if (book.state > 30) {
+        this.books.push(book);
+      }
+    }
+  
+    findBookBy(type, value) {
+      return this.books.find(book => book[type] === value) || null;
+    }
+  
+    giveBookByName(bookName) {
+      const bookIndex = this.books.findIndex(book => book.name === bookName);
+      if (bookIndex !== -1) {
+        return this.books.splice(bookIndex, 1)[0];
+      }
+      return null;
+    }
+  }
+
+  const library = new Library("Библиотека имени Ленина");
+  
+  library.addBook(
+    new DetectiveBook(
+      "Артур Конан Дойл",
+      "Полное собрание повестей и рассказов о Шерлоке Холмсе в одном томе",
+      2019,
+      1008
+    )
   );
+  library.addBook(
+    new FantasticBook(
+      "Аркадий и Борис Стругацкие",
+      "Пикник на обочине",
+      1972,
+      168
+    )
+  );
+  library.addBook(new NovelBook("Герберт Уэллс", "Машина времени", 1895, 138));
+  library.addBook(new Magazine("Мурзилка", 1924, 60));
   
-  console.log(picknick.author); // "Аркадий и Борис Стругацкие"
-  picknick.state = 10;
-  console.log(picknick.state); // 10
-  picknick.fix();
-  console.log(picknick.state); // 15
+  console.log(library.findBookBy("name", "Властелин колец")); 
+  console.log(library.findBookBy("releaseDate", 1924).name); 
   
+  console.log("Количество книг до выдачи: " + library.books.length);
+  const borrowedBook = library.giveBookByName("Машина времени");
+  console.log("Количество книг после выдачи: " + library.books.length);
+  
+  if (borrowedBook) {
+    borrowedBook.state = 10;
+    console.log(borrowedBook.state); 
+    borrowedBook.fix();
+    console.log(borrowedBook.state); 
+    library.addBook(borrowedBook);
+    console.log("Количество книг после возврата: " + library.books.length);
+  }
